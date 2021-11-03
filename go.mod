ddcd381fcdcfb062e3d6d7ed423319d892cf938d
Huy Duc Dao <ducdh.starvn@gmail.com>
2021-11-03 17:54:12 +0700

    Config parser for the API Gateway

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
 	github.com/rs/cors v1.6.0
 	github.com/sony/gobreaker v0.5.0
+	github.com/spf13/viper v1.9.0
 	github.com/starvn/turbo v1.0.0
 	github.com/unrolled/secure v1.0.9
 	github.com/yuin/gopher-lua v0.0.0-20190206043414-8bfc7677f583
@@ -21,19 +22,29 @@ require (
 	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20210826220005-b48c857c3a0e // indirect
 	github.com/dlclark/regexp2 v1.4.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/starvn/flatex v1.0.2 // indirect
 	github.com/stoewer/go-strcase v1.2.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
 	github.com/valyala/fastrand v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
@@ -41,5 +52,6 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20210831024726-fe130286e0e2 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/ini.v1 v1.63.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ viper/parser_test.go
@@ -0,0 +1,426 @@
+/*
+ * Copyright (c) 2021 Huy Duc Dao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viper
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestNew_ok(t *testing.T) {
+	configContents := []struct {
+		format  string
+		path    string
+		content []byte
+	}{
+		{"json", "/tmp/ok.json", jsonCfg},
+		{"toml", "/tmp/ok.toml", tomlCfg},
+		{"yaml", "/tmp/ok.yml", yamlCfg},
+	}
+	for _, configContent := range configContents {
+		t.Run(configContent.format, func(t *testing.T) {
+
+			if err := ioutil.WriteFile(configContent.path, configContent.content, 0644); err != nil {
+				t.FailNow()
+			}
+
+			serviceConfig, err := New().Parse(configContent.path)
+			if err != nil {
+				t.Error("Unexpected error. Got", err.Error())
+			}
+
+			endpoint := serviceConfig.Endpoints[0]
+			endpointExtraConfiguration := endpoint.ExtraConfig
+
+			if endpointExtraConfiguration != nil {
+				testExtraConfig(endpointExtraConfiguration, t)
+			} else {
+				t.Error("Extra config is not present in EndpointConfig")
+			}
+
+			backend := endpoint.Backend[0]
+			backendExtraConfiguration := backend.ExtraConfig
+			if backendExtraConfiguration != nil {
+				testExtraConfig(backendExtraConfiguration, t)
+			} else {
+				t.Error("Extra config is not present in BackendConfig")
+			}
+
+			if err := os.Remove(configContent.path); err != nil {
+				t.FailNow()
+			}
+		})
+	}
+}
+
+func TestNew_errorMessages(t *testing.T) {
+	for _, configContent := range []struct {
+		name    string
+		path    string
+		content []byte
+		expErr  string
+	}{
+		{
+			name:    "case0",
+			path:    "/tmp/ok.json",
+			content: []byte(`{`),
+			expErr:  "'/tmp/ok.json': unexpected end of JSON input, offset: 1, row: 0, col: 1",
+		},
+		{
+			name:    "case1",
+			path:    "/tmp/ok.json",
+			content: []byte(`>`),
+			expErr:  "'/tmp/ok.json': invalid character '>' looking for beginning of value, offset: 1, row: 0, col: 1",
+		},
+		{
+			name:    "case2",
+			path:    "/tmp/ok.json",
+			content: []byte(`"`),
+			expErr:  "'/tmp/ok.json': unexpected end of JSON input, offset: 1, row: 0, col: 1",
+		},
+		{
+			name:    "case3",
+			path:    "/tmp/ok.json",
+			content: []byte(``),
+			expErr:  "'/tmp/ok.json': unexpected end of JSON input, offset: 0, row: 0, col: 0",
+		},
+		{
+			name:    "case4",
+			path:    "/tmp/ok.json",
+			content: []byte(`[{}]`),
+			expErr:  "'/tmp/ok.json': json: cannot unmarshal array into Go value of type map[string]interface {}, offset: 1, row: 0, col: 1",
+		},
+		{
+			name:    "case5",
+			path:    "/tmp/ok.json",
+			content: []byte(`42`),
+			expErr:  "'/tmp/ok.json': json: cannot unmarshal number into Go value of type map[string]interface {}, offset: 2, row: 0, col: 2",
+		},
+		{
+			name:    "case6",
+			path:    "/tmp/ok.json",
+			content: []byte("\r\n42"),
+			expErr:  "'/tmp/ok.json': json: cannot unmarshal number into Go value of type map[string]interface {}, offset: 4, row: 1, col: 2",
+		},
+		{
+			name: "case7",
+			path: "/tmp/ok.json",
+			content: []byte(`{
+	"version": 2,
+	"name": "My lovely gateway",
+	"port": 8080,
+	"cache_ttl": 3600
+	"timeout": "3s",
+	"endpoints": []
+}`),
+			expErr: "'/tmp/ok.json': invalid character '\"' after object key:value pair, offset: 83, row: 5, col: 2",
+		},
+	} {
+		t.Run(configContent.name, func(t *testing.T) {
+			if err := ioutil.WriteFile(configContent.path, configContent.content, 0644); err != nil {
+				t.Error(err)
+				return
+			}
+
+			_, err := New().Parse(configContent.path)
+			if err == nil {
+				t.Errorf("%s: Expecting error", configContent.name)
+				return
+			}
+			if errMsg := err.Error(); errMsg != configContent.expErr {
+				t.Errorf("%s: Unexpected error. Got '%s' want '%s'", configContent.name, errMsg, configContent.expErr)
+				return
+			}
+
+			if err := os.Remove(configContent.path); err != nil {
+				t.Errorf("%s: %s", err.Error(), configContent.name)
+				return
+			}
+		})
+	}
+}
+
+func testExtraConfig(extraConfig map[string]interface{}, t *testing.T) {
+	userVar := extraConfig["user"]
+	if userVar != "test" {
+		t.Error("User in extra config is not test")
+	}
+	parents := extraConfig["parents"].([]interface{})
+	if parents[0] != "starvn" {
+		t.Error("Parent 0 of user us not starvn")
+	}
+	if parents[1] != "ducdh" {
+		t.Error("Parent 1 of user us not ducdh")
+	}
+
+	testExtraNestedConfigKey(extraConfig, t)
+}
+
+func testExtraNestedConfigKey(extraConfig map[string]interface{}, t *testing.T) {
+	namespace := "nested_data"
+	v, ok := extraConfig[namespace]
+	if !ok {
+		return
+	}
+
+	type nestedConfig struct {
+		Data struct {
+			Status string `json:"status"`
+		} `json:"data"`
+	}
+
+	jsonBytes, err := json.Marshal(v)
+	if err != nil {
+		t.Error("marshal nested config key error: ", err.Error())
+		return
+	}
+
+	var cfg nestedConfig
+	if err = json.Unmarshal(jsonBytes, &cfg); err != nil {
+		t.Error("unmarshal nested config key error: ", err.Error())
+		return
+	}
+
+	if cfg.Data.Status != "OK" {
+		t.Errorf("nested config key parse error: %+v\n", cfg)
+	}
+}
+
+func TestNew_unknownFile(t *testing.T) {
+	_, err := New().Parse("/nowhere/in/the/fs.json")
+	if err == nil || err.Error() != "'/nowhere/in/the/fs.json' (open): no such file or directory" {
+		t.Errorf("Error expected. Got '%s'", err)
+	}
+}
+
+func TestNew_readingError(t *testing.T) {
+	wrongConfigPath := "/tmp/reading.json"
+	wrongConfigContent := []byte("{hello\ngo\n")
+	if err := ioutil.WriteFile(wrongConfigPath, wrongConfigContent, 0644); err != nil {
+		t.FailNow()
+	}
+
+	expected := "'/tmp/reading.json': invalid character 'h' looking for beginning of object key string, offset: 2, row: 0, col: 2"
+	_, err := New().Parse(wrongConfigPath)
+	if err == nil || err.Error() != expected {
+		t.Errorf("Error expected. Got '%s'", err)
+	}
+	if err = os.Remove(wrongConfigPath); err != nil {
+		t.FailNow()
+	}
+}
+
+func TestNew_initError(t *testing.T) {
+	wrongConfigPath := "/tmp/unmarshall.json"
+	wrongConfigContent := []byte("{\"a\":42}")
+	if err := ioutil.WriteFile(wrongConfigPath, wrongConfigContent, 0644); err != nil {
+		t.FailNow()
+	}
+
+	_, err := New().Parse(wrongConfigPath)
+	if err == nil || err.Error() != "'/tmp/unmarshall.json': unsupported version: 0 (want: 1)" {
+		t.Error("Error expected. Got", err)
+	}
+	if err = os.Remove(wrongConfigPath); err != nil {
+		t.FailNow()
+	}
+}
+
+var (
+	jsonCfg = []byte(`{
+	"version": 1,
+	"name": "Sonic API Gateway",
+	"port": 8080,
+	"cache_ttl": "3600s",
+	"timeout": "3s",
+	"endpoints": [
+			{
+					"endpoint": "/github",
+					"method": "GET",
+					"extra_config" : {"user":"test","hits":6,"parents":["starvn","ducdh"], "nested_data": {"data": {"status": "OK"}}},
+					"backend": [
+							{
+									"host": [
+											"https://api.github.com"
+									],
+									"url_pattern": "/",
+									"whitelist": [
+											"authorizations_url",
+											"code_search_url"
+									],
+									"extra_config" : {"user":"test","hits":6,"parents":["starvn","ducdh"]}
+							}
+					]
+			},
+			{
+					"endpoint": "/sonic",
+					"method": "GET",
+					"concurrent_calls": 3,
+					"backend": [
+							{
+									"host": [
+											"http://127.0.0.1:8080"
+									],
+									"url_pattern": "/__debug/sonic"
+							}
+					]
+			},
+			{
+					"endpoint": "/combination/{id}",
+					"method": "GET",
+					"backend": [
+							{
+									"group": "first_post",
+									"host": [
+											"https://jsonplaceholder.typicode.com"
+									],
+									"url_pattern": "/posts/{id}",
+									"blacklist": [
+											"userId"
+									]
+							},
+							{
+									"host": [
+											"https://jsonplaceholder.typicode.com"
+									],
+									"url_pattern": "/users/{id}",
+									"mapping": {
+											"email": "personal_email"
+									}
+							}
+					]
+			}
+	]
+}`)
+
+	tomlCfg = []byte(`version = 1
+name = "Sonic API Gateway"
+port = 8080.0
+cache_ttl = "3600s"
+timeout = "3s"
+[[endpoints]]
+endpoint = "/github"
+method = "GET"
+[endpoints.extra_config]
+user = "test"
+hits = 6.0
+parents = [
+"starvn",
+"ducdh"
+]
+[endpoints.extra_config.nested_data.data]
+status = "OK"
+[[endpoints.backend]]
+host = [
+"https://api.github.com"
+]
+url_pattern = "/"
+whitelist = [
+"authorizations_url",
+"code_search_url"
+]
+[endpoints.backend.extra_config]
+user = "test"
+hits = 6.0
+parents = [
+	"starvn",
+	"ducdh"
+]
+[[endpoints]]
+endpoint = "/sonic"
+method = "GET"
+concurrent_calls = 3.0
+[[endpoints.backend]]
+host = [
+"http://127.0.0.1:8080"
+]
+url_pattern = "/__debug/sonic"
+[[endpoints]]
+endpoint = "/combination/{id}"
+method = "GET"
+[[endpoints.backend]]
+group = "first_post"
+host = [
+"https://jsonplaceholder.typicode.com"
+]
+url_pattern = "/posts/{id}"
+blacklist = [
+"userId"
+]
+[[endpoints.backend]]
+host = [
+"https://jsonplaceholder.typicode.com"
+]
+url_pattern = "/users/{id}"
+[endpoints.backend.mapping]
+email = "personal_email"`)
+
+	yamlCfg = []byte(`version: 1
+name: Sonic API Gateway
+port: 8080
+cache_ttl: 3600s
+timeout: 3s
+endpoints:
+  - endpoint: /github
+    method: GET
+    extra_config:
+      user: test
+      hits: 6
+      parents:
+        - starvn
+        - ducdh
+      nested_data:
+        data:
+          status: OK
+    backend:
+      - host:
+          - 'https://api.github.com'
+        url_pattern: /
+        whitelist:
+          - authorizations_url
+          - code_search_url
+        extra_config:
+          user: test
+          hits: 6
+          parents:
+            - starvn
+            - ducdh
+  - endpoint: /sonic
+    method: GET
+    concurrent_calls: 3
+    backend:
+      - host:
+          - 'http://127.0.0.1:8080'
+        url_pattern: /__debug/sonic
+  - endpoint: '/combination/{id}'
+    method: GET
+    backend:
+      - group: first_post
+        host:
+          - 'https://jsonplaceholder.typicode.com'
+        url_pattern: '/posts/{id}'
+        blacklist:
+          - userId
+      - host:
+          - 'https://jsonplaceholder.typicode.com'
+        url_pattern: '/users/{id}'
+        mapping:
+          email: personal_email
+`)
+)
