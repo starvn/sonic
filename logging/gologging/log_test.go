f1ee894f41298e608e0e3f858228d4c9cbfdf26f
Huy Duc Dao <ducdh.starvn@gmail.com>
2021-11-04 01:33:47 +0700

    Adds Sonic logging

---
+++ logging/gologging/log_test.go
@@ -0,0 +1,161 @@
+//go:build !windows && !plan9
+// +build !windows,!plan9
+
+/*
+ * Copyright (c) 2021 Huy Duc Dao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gologging
+
+import (
+	"bytes"
+	"encoding/json"
+	gologging "github.com/op/go-logging"
+	"io"
+	"regexp"
+	"strings"
+	"testing"
+)
+
+const (
+	debugMsg    = "Debug msg"
+	infoMsg     = "Info msg"
+	warningMsg  = "Warning msg"
+	errorMsg    = "Error msg"
+	criticalMsg = "Critical msg"
+)
+
+func TestNewLogger(t *testing.T) {
+	levels := []string{"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"}
+	regexps := []*regexp.Regexp{
+		regexp.MustCompile(debugMsg),
+		regexp.MustCompile(infoMsg),
+		regexp.MustCompile(warningMsg),
+		regexp.MustCompile(errorMsg),
+		regexp.MustCompile(criticalMsg),
+	}
+
+	for i, level := range levels {
+		output, err := logSomeStuff(level)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		for j := i; j < len(regexps); j++ {
+			if !regexps[j].MatchString(output) {
+				t.Errorf("The output doesn't contain the expected msg for the level: %s. [%s]", level, output)
+			}
+		}
+	}
+}
+
+func TestNewLogger_logstashFormat(t *testing.T) {
+	buff := bytes.NewBuffer(make([]byte, 1024))
+	SetFormatterSelector(func(w io.Writer) string {
+		return ActivePattern
+	})
+	logger, err := NewLogger(newExtraConfig("DEBUG", "logstash", ""), TestFormatWriter{buff})
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	logger.Critical(criticalMsg)
+
+	outputMsg := strings.Replace(buff.String(), "\x00", "", -1)
+
+	if !isJson(outputMsg) || !strings.HasPrefix(outputMsg, "{\"@timestamp\":") {
+		t.Error("The output doesn't contain a logstash formatted log line")
+	}
+}
+func TestNewLogger_customFormat(t *testing.T) {
+	buff := bytes.NewBuffer(make([]byte, 1024))
+	SetFormatterSelector(func(w io.Writer) string {
+		return ActivePattern
+	})
+	logger, err := NewLogger(newExtraConfig("DEBUG", "custom", "----> %{message}"), TestFormatWriter{buff})
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	logger.Critical(criticalMsg)
+
+	outputMsg := strings.Replace(buff.String(), "\x00", "", -1)
+
+	if "----> Critical msg\n" != outputMsg {
+		t.Error("The output doesn't contain the custom format")
+	}
+}
+
+func TestNewLogger_unknownLevel(t *testing.T) {
+	_, err := NewLogger(newExtraConfig("UNKNOWN", "default", ""), bytes.NewBuffer(make([]byte, 1024)))
+	if err == nil {
+		t.Error("The factory didn't return the expected error")
+		return
+	}
+	if err != gologging.ErrInvalidLogLevel {
+		t.Errorf("The factory didn't return the expected error. Got: %s", err.Error())
+	}
+}
+
+func newExtraConfig(level string, format string, customFormat string) map[string]interface{} {
+	return map[string]interface{}{
+		Namespace: map[string]interface{}{
+			"level":         level,
+			"prefix":        "pref",
+			"syslog":        false,
+			"stdout":        true,
+			"format":        format,
+			"custom_format": customFormat,
+		},
+	}
+}
+
+type TestFormatWriter struct {
+	io.Writer
+}
+
+func logSomeStuff(level string) (string, error) {
+	buff := bytes.NewBuffer(make([]byte, 1024))
+	SetFormatterSelector(func(w io.Writer) string {
+		switch w.(type) {
+		case TestFormatWriter:
+			return "customFormatter %{message}"
+		default:
+			return DefaultPattern
+		}
+	})
+	logger, err := NewLogger(newExtraConfig(level, "default", ""), TestFormatWriter{buff})
+	if err != nil {
+		return "", err
+	}
+
+	logger.Debug(debugMsg)
+	logger.Info(infoMsg)
+	logger.Warning(warningMsg)
+	logger.Error(errorMsg)
+	logger.Critical(criticalMsg)
+
+	return buff.String(), nil
+}
+
+func isJson(possibleJson string) bool {
+	var js map[string]interface{}
+
+	return json.Unmarshal([]byte(possibleJson), &js) == nil
+}
